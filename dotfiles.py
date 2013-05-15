0d6dd4ada3181fea59eb9023bcede07e1e8700fd
Mike Walker <noisufnoc@gmail.com>
2013-05-15 10:14:53 -0700

    more logic.

--- dotfiles.py
+++ dotfiles.py
@@ -3,6 +3,7 @@
 __author__ = 'noisufnoc'
 
 # Link dotfiles to .dotfiles for managing with git
+# Similar to https://github.com/michaeljsmalley/dotfiles, but in py.
 
 import os
 
@@ -12,15 +13,21 @@ OLDDIR = '%s/.dotfiles_old' % HOMEDIR
 FILES = []
 
 # Backup existing files in OLDDIR
-#os.rename()
-#os.makedirs(OLDDIR)
+if not os.path.exists(OLDDIR):
+    os.makedirs(OLDDIR)
+
 
 print HOMEDIR
 print DIR
 print OLDDIR
 
 # mv files and create symlinks
+if not os.path.exists(DIR):
+    os.makedirs(DIR)
+
+
 #os.symlink()
+#os.rename()
 
 # done, this script is done.
 
