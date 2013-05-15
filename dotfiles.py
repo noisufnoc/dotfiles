5a1ca9ff3be02d4364b15ea6a2d2bb2991495cb1
Mike Walker <noisufnoc@gmail.com>
2013-05-15 08:41:14 -0700

    Initial commit.

---
+++ dotfiles.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+__author__ = 'noisufnoc'
+
+DIR = '~/.dotfiles'
+OLDDIR = '~/.dotfiles_old'
+FILES = []
+
+# Backup existing files in OLDDIR
+
+# mv files and create symlinks
+
+# done, this script is done.
+
+
