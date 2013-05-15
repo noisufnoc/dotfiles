41d75c7adc09fff57c31f1626d709dba0672c1fa
Mike Walker <noisufnoc@gmail.com>
2013-05-15 11:21:05 -0700

    I think this will work.  PRINT ALL THE THINGS!

--- dotfiles.py
+++ dotfiles.py
@@ -10,22 +10,32 @@ import os
 HOMEDIR = os.path.expanduser('~')
 DIR = '%s/.dotfiles' % HOMEDIR
 OLDDIR = '%s/.dotfiles_old' % HOMEDIR
-FILES = []
+FILES = ['foo', 'bar']
 
 # Backup existing files in OLDDIR
 if not os.path.exists(OLDDIR):
     os.makedirs(OLDDIR)
 
-
-print HOMEDIR
-print DIR
-print OLDDIR
-
 # mv files and create symlinks
 if not os.path.exists(DIR):
     os.makedirs(DIR)
 
+for file in FILES:
+    file_orig = '%s/.%s' % (HOMEDIR, file)
+    file_back = '%s/%s' % (OLDDIR, file)
+    file_link = '%s/%s' % (DIR, file)
 
+    print 'I\'m moving %s' % file_orig
+    print 'New Path: %s' % file_back
+    print 'symlink %s to %s' % (file_link, file_orig)
+
+    #os.rename(file_orig, file_back)
+    #os.symlink(file_link, file_orig)
+
+
+#print HOMEDIR
+#print DIR
+#print OLDDIR
 #os.symlink()
 #os.rename()
 
