70fafb60bf49582b97ac9e1e5c1050a5b1169196
Mike Walker <noisufnoc@gmail.com>
2015-09-08 19:23:07 -0400

    syncing this up from bitbucket

--- dotfiles.py
+++ dotfiles.py
@@ -3,41 +3,58 @@
 __author__ = 'noisufnoc'
 
 # Link dotfiles to .dotfiles for managing with git
-# Similar to https://github.com/michaeljsmalley/dotfiles, but in py.
 
 # TODO improve zsh installer
 # TODO put private dotfiles in private repo, publish public safe stuff
+# TODO private dotfiles could be in a folder, that's cloned from priv repo
 # TODO probably should obey pep8
+# TODO logging
+# TODO test mode
 
 import os
 import sys
-import platform
+from platform import system, system_alias, release, version, linux_distribution
 
 if sys.version_info < (2, 6):
     print "I require 2.6 or higher"
     sys.exit(1)
 
+# Determine OS type so I can use specific dotfiles
+#     think about how to set this variable
+
+_os, release, version = system_alias(system(), release(), version())
+if _os.lower() == 'linux':
+    myos = 'linux'
+elif _os.lower() == 'darwin':
+    myos = 'osx'
+elif _os.lower() == 'win32':
+    myos = 'windows'
+else:
+    # i dunno you should check this first
+    print "I'm not sure what OS you're running, lets quit."
+    sys.exit(1)
+
 HOMEDIR = os.path.expanduser('~')
 DIR = '%s/.dotfiles' % HOMEDIR
 OLDDIR = '%s/.dotfiles_old' % HOMEDIR
-FILES = [
-    'fonts',
-    'i3',
-    'i3status.conf',
-    'inputrc',
-    'jira-url',
-    'screenrc',
-    'tmux.conf',
-    'turses',
-    'vim',
-    'vimrc',
-    'virtualenvs',
-    'weechat',
-    'Xresources',
-    'zshenv',
-    'zshrc'
-]
+SECRETS = 'url of secret repo'  # TODO set this, obv
 
+# here you should build the list of secret files from priv git repo
+# if not os.path.exists('secret'):
+#    os.makedirs('secret')
+#    os.system('git clone %s' % SECRETS)
+
+
+# I need to get the files in the folders and build the FILES list
+
+cp_files = ['crossplatform/{0}'.format(i) for i in os.listdir('%s/crossplatform' % DIR)]
+secret_files = ['secret/{0}'.format(i) for i in os.listdir('%s/secret' % DIR)]
+os_files = [('%s/{0}' % myos).format(i) for i in os.listdir('%s/%s' % (DIR, myos))]
+
+FILES = cp_files + secret_files + os_files
+
+for file in FILES:
+    print '%s/.%s linked to %s/%s' % (HOMEDIR, file.split('/')[1], DIR, file)
 
 # Backup existing files in OLDDIR
 if not os.path.exists(OLDDIR):
@@ -49,10 +66,11 @@ if not os.path.exists(DIR):
 
 for file in FILES:
     # Iterate through file list
-    file_orig = '%s/.%s' % (HOMEDIR, file)
-    file_back = '%s/%s' % (OLDDIR, file)
+    file_orig = '%s/.%s' % (HOMEDIR, file.split('/')[1])
+    file_back = '%s/%s' % (OLDDIR, file.split('/')[1])
     file_link = '%s/%s' % (DIR, file)
 
+    # This should never fire
     if not os.path.exists(file_link):
         print 'You listed file \'%s\' that doesn\'t exist in %s, Skipping...' % (file, DIR)
         continue
@@ -72,16 +90,16 @@ for file in FILES:
 
 # I use vundle, you should too.
 
-if os.path.exists('%s/vim/bundle' % DIR):
-    os.system('git clone https://github.com/gmarik/vundle.git %s/vim/bundle/vundle' % DIR)
+if os.path.exists('%s/.vim/bundle' % HOMEDIR):
+    os.system('git clone https://github.com/gmarik/vundle.git %s/.vim/bundle/vundle' % HOMEDIR)
     os.system('vim +PluginInstall +qall')
 
 # I really like zsh and oh-my-zsh
 
 if not os.path.exists('/bin/zsh') or os.path.exists('/usr/bin/zsh'):
-    if platform.linux_distribution()[0] == 'Ubuntu':
+    if linux_distribution()[0] == 'Ubuntu':
         os.system('sudo apt-get install -y zsh')
-    elif platform.linux_distribution()[0] == 'CentOS':
+    elif linux_distribution()[0] == 'CentOS':
         os.system('sudo yum install -y zsh')
     else:
         print 'I don\'t know what distro you\'re running, install zsh manually!'
